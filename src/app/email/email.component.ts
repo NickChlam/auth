fd1882bc1e1a33ae2f724bd8de6a9263f85db236
NickChlam <Nick.Chlam@rht.com>
2018-03-18 08:57:16 -0600

    email component

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,13 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+//import {movein, fallIn } from '../router.animations';
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
-  styleUrls: ['./email.component.css']
+  styleUrls: ['./email.component.css'],
+  //animations: [moveIn(), fallIn()],
+  //host: {'[@moveIn]': ''}
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+
+  constructor(public af: AngularFireAuth, private router: Router) {
+    this.af.authState.subscribe(auth => {
+      if(auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+   }
+   
+   onSubmit(formData) {
+     if(formData.valid){
+       console.log(formData.value);
+       this.af.auth.createUserAndRetrieveDataWithEmailAndPassword(formData.value.email, FormData.value.password)
+       .then(
+         (success) => {
+           console.log(success);
+           this.router.navigate(['/members']);
+
+         }).catch(
+           (err) => {
+             console.log(err);
+             this.error = err;
+           })
+     }
+   }
+
 
   ngOnInit() {
   }
